9e96c0459b670ed916939b9d4bfc39c13d607855
Cadominguez09 <123121857+Cadominguez09@users.noreply.github.com>
2026-06-10 15:20:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -395,7 +395,7 @@ if not st.session_state.logueado:
 if st.session_state.logueado:
 
     st.success(f"Bienvenido/a, {st.session_state.usuario} ⚽")
-
+    st.write("ADMIN:", st.session_state.es_admin)
     if st.button("Cerrar sesión"):
         st.session_state.logueado = False
         st.session_state.usuario = ""
