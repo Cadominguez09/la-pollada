9769cf7153f129b850aaccf063750af209e45fcb
Cadominguez09 <123121857+Cadominguez09@users.noreply.github.com>
2026-06-13 11:07:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -803,16 +803,16 @@ if st.session_state.logueado:
 
             resumen = ""
 
-         for p in pronosticos:
+          for p in pronosticos:
             resumen += (
             f"{p['equipo_local']} {p['goles_local']}-"
             f"{p['goles_visitante']} {p['equipo_visitante']}\n"
     )
 
-         st.text_area(
-         "Copia o toma captura de este resumen",
-         resumen,
-         height=300
+          st.text_area(
+          "Copia o toma captura de este resumen",
+          resumen,
+          height=300
 )   
     with tab2:
 
